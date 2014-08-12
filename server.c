6a1ef9b560d8c0c6447e8322e9b6f5691a480aa0
xiaoyunzhi <lyunofscu@gmail.com>
2014-08-12 20:00:33 +0800

    need to complete concurrency

--- server.c
+++ server.c
@@ -11,8 +11,9 @@ typedef struct {
     int  flag_userpid_valid;
     int  flag_globalmode_valid;
     int  charac_handled_count;
-    char mode_global[MODE_LEN];
-    char mode_self[MODE_LEN];
+    u32  user_pid;
+    int  mode_global;           /* 1: normal (default mode), 2: upper, 3: lower */
+    int  mode_self;   /* 1: normal (default mode), 2: upper, 3: lower */
 } userpid_mode_t;
 
 struct task_struct *proc_tsk;
@@ -22,7 +23,10 @@ static char *data_buf;
 static struct proc_dir_entry *echo_proc_dir, *echo_proc_file;
 static struct sock *nl_sk;
 static unsigned int flag_exit;
+static unsigned int client_count;
+static unsigned int global_charac_handled;
 
+/* show the message to user space */
 static int proc_read_echo(char *page, char **start, off_t off, int count, int *eof)
 {
     int len;
@@ -31,10 +35,12 @@ static int proc_read_echo(char *page, char **start, off_t off, int count, int *e
     return len;
 }
 
+ /* get data from user data, receive the data and the check if user pid is exited */
 static int proc_write_echo(struct file *file, const char *buf, unsigned int count, void *data)
 {
-
+   
 }
+
 static int proc_tsk_handle(void *data)
 {
     echo_proc_dir = proc_mkdir(ECHO_DIR, NULL);
@@ -48,34 +54,110 @@ static void nl_data_wake(struct sock *sk, int len)
     wake_up_interruptibale(sk->sk_sleep);
 }
 
+static void normal_feedback()
+{
+
+}
+
+static void upper_feedback()
+{
+
+}
+
+static void lower_feedback()
+{
+
+}
+
+static void create_new_userpid()
+{
+
+}
+
 static int netlink_tsk_handle(void *data)
 {
     static struct sk_buf *skb;
-    static struct nlmsghdr *nlk;
-    int err, i;
+    static struct nlmsghdr *nlh;
+    int err, i, place_idle;
     u32 pid;
     
     nl_sk = netlink_kernel_create(NETLINK_TEST, nl_data_wake);
-    do {
-        skb = NULL;
-        skb = skb_recv_datagram(nl_sk, 0, 0, &err);
+   
+//        skb = skb_recv_datagram(nl_sk, 0, 0, &err);
+    nlh = (struct nlmsghdr *)skb->data;
+    if (client_count < CLIENT_MAX) {
         for (i = 0; i < CLIENT_MAX; i++) {
             if (userpid_mode_arr[i]) {
-                userpid_mode_arr[i]->
+                if (userpid_mode_arr[i]->user_pid == nlh->nlmsg_pid) {
+                    /* the user_pid in being in the arr and global mode is valid*/
+                    if (userpid_mode_arr[i]->flag_globalmode_valid) {  
+                        switch (userpid_mode_arr[i]->mode_global) {
+                        case 1:
+                            normal_feedback();
+                            break;
+                        case 2:
+                            upper_feedback();
+                            break;
+                        case 3:
+                            lower_feedback();
+                            break;
+                        default:
+                            break;
+                        }
+                    } else {
+                        switch (userpid_mode_arr[i]->mode_self) {
+                        case 1:
+                            normal_feedback();
+                            break;
+                        case 2:
+                            upper_feedback();
+                            break;
+                        case 3:
+                            lower_feedback();
+                            break;
+                        default:
+                            break;
+                        }
+                    }
+                }
+            } else {
+                place_idle = i;
             }
-        }
-    } while (flag_exit)
+            /* need to kmalloc a buf to fill up the new user process and client_count++*/
+            create_new_userpid();
+        }        
+    } else {
+        printk("the count of client reach max\n");
+    }
 }
 
 static int __init echo_init(void)
 {
     flag_exit = 1;
     proc_tsk = kthread_run(proc_tsk_handle, NULL, "proc_tsk_handle");
-    netlink_tsk = thread_run(netlink_tsk_handle, NULL, "netlink_tsk_handle");
+    nl_sk = netlink_kernel_create(NETLINK_TEST, nl_data_wake);
+    while (flag_exit) {
+        skb = NULL;
+        skb = skb_dequeue(&sk->receive_queue)
+        if (skb == NULL) {
+            continue;
+        }
+        netlink_tsk = thread_run(netlink_tsk_handle, (void *)skb, "netlink_tsk_handle");
+    }
+    
 }
 
 static void __exit echo_exit(void)
 {
+    flag_exit = 0;
+    if (!IS_ERR(proc_tsk)) {
+        kthread_stop(proc_tsk);
+    }
+
+    if (!IS_ERR(proc_tsk)) {
+        kthread_stop(netlink_tsk);
+    }
+
     if (echo_proc_file) {
         remove_proc_entry(ECHO_FILE, echo_proc_dir);
     }
